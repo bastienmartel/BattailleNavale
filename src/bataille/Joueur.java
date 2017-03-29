5be216ca734671ff067be82c3590c8655ee326ae
JACQUEMIN GAETAN p1401224 <p1401224@univ-lyon1.fr>
2017-03-29 17:41:04 +0200

    Classe Joueur complétée

--- src/bataille/Joueur.java
+++ src/bataille/Joueur.java
@@ -2,4 +2,33 @@ package bataille;
 
 public class Joueur {
 
+		private String nomJoueur;
+		private int score;
+		
+		
+		
+		public Joueur(String nomJoueur, int score)
+		{
+			this.nomJoueur = nomJoueur;
+			this.score = score;
+		}
+		public String getNomJoueur()
+		{
+			return nomJoueur;
+		}
+		public void setNomJoueur(String nomJoueur)
+		{
+			this.nomJoueur = nomJoueur;
+		}
+		public int getScore()
+		{
+			return score;
+		}
+		public void setScore(int score)
+		{
+			this.score = score;
+		}
+		
+		
+		
 }
