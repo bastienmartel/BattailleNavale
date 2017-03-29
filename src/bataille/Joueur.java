022081f7394c67d4aa0273acc7ab68f45baa3663
PhengDylan <ph3ng.dylan@gmail.com>
2017-03-29 17:56:25 +0200

    Modif Joueur

--- src/bataille/Joueur.java
+++ src/bataille/Joueur.java
@@ -4,13 +4,16 @@ public class Joueur {
 
 		private String nomJoueur;
 		private int score;
+		private Grille grilleJoueur;
+		private Bateau bateau;
 		
 		
 		
-		public Joueur(String nomJoueur, int score)
+		public Joueur(String nomJoueur, int score, int width, int heigth )
 		{
 			this.nomJoueur = nomJoueur;
 			this.score = score;
+			grilleJoueur = new Grille(width,heigth);
 		}
 		public String getNomJoueur()
 		{
