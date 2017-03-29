b71c8691c51679f2cf50d536abcc991190e02369
PhengDylan <ph3ng.dylan@gmail.com>
2017-03-29 17:19:18 +0200

    Rajout des Test

---
+++ src/test/TestTemplate.java
@@ -0,0 +1,29 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import bataille.Grille;
+
+public class TestTemplate {
+
+	Grille g;
+	
+	@org.junit.Before
+	public void setup()
+	{
+		g = new Grille(5, 6);
+	}
+	
+	@org.junit.Test
+	public void test() {
+		
+		
+		
+	}
+	@org.junit.Test
+	public void test2()
+	{
+		
+	}
+
+}
