e8cdd10257022b9abbc5203740730308ce38ac1a
gprog39 <pyfes@aol.com>
2017-03-30 10:46:13 +0200

    modifs testTemplate

--- src/test/TestTemplate.java
+++ src/test/TestTemplate.java
@@ -1,27 +1,30 @@
 package test;
 
 import static org.junit.Assert.*;
-
+import bataille.Bateau;
 import bataille.Grille;
 
 public class TestTemplate {
 
 	Grille g;
+	Bateau b;
 	
 	@org.junit.Before
 	public void setup()
 	{
 		g = new Grille(5, 6);
+		b= new Bateau(1,5); //porte avion de taille 5
+		
 	}
 	
 	@org.junit.Test
-	public void test() {
+	public void testInsertionGrille() {
 		
 		
 		
 	}
 	@org.junit.Test
-	public void test2()
+	public void test()
 	{
 		
 	}
