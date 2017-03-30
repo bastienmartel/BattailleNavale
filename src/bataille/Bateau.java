ffcd9133df7c4dc9d8c5552f5643d7fb1b8eafdc
gprog39 <pyfes@aol.com>
2017-03-30 11:33:54 +0200

    modif Bateau avec x1,y1 et x2,y2

--- src/bataille/Bateau.java
+++ src/bataille/Bateau.java
@@ -10,15 +10,29 @@ package bataille;
 public class Bateau {
 	private static String[] typeBateau = {"Porte Avion","Croiseur","Contre Torpilleurs","Sous Marin","Torpilleur"};
 	private int taille;
-	private String bateau;
-	private int x;
-	private int y;
+	private String bateau; // Nom du Bateau
+	private int x1, y1, x2, y2;
 	
+	private int toucher;
 	
-	public Bateau(int indice, int taille) {
+	/**
+	 * 
+	 * @param indice Indice du Bateau 1:PorteAvion 2:Croiseur 3:ContreTorpilleur 4:SousMarin 6:Torpilleur
+	 * @param taille Taille du bateau
+	 * @param x1  abscisse début du bateau
+	 * @param y1  ordonnée début du bateau
+	 * @param x2  abscisse fin du bateau
+	 * @param y2  ordonnée fin du bateau
+	 * 
+	 */
+	public Bateau(int indice, int taille,int x1, int y1, int x2, int y2) {
 		super();
 		this.bateau = typeBateau[indice];
 		this.taille = taille;
+		this.x1= x1;
+		this.y1= y1;
+		this.x2 = x2;
+		this.y2 = y2;
 	}
 	
 	
@@ -26,7 +40,7 @@ public class Bateau {
 		return typeBateau;
 	}
 	public void setTypeBateau(String[] typeBateau) {
-		this.typeBateau = typeBateau;
+		Bateau.typeBateau = typeBateau;
 	}
 	public int getTaille() {
 		return taille;
@@ -60,6 +74,8 @@ public class Bateau {
 	public String toString() {
 		return "Le " + bateau + " de taille " + taille + "entre dans le jeu"  ;
 	}
+	
+	
 	
 	
 
--- src/test/TestTemplate.java
+++ src/test/TestTemplate.java
@@ -3,18 +3,19 @@ package test;
 import static org.junit.Assert.*;
 import bataille.Bateau;
 import bataille.Grille;
+import bataille.Joueur;
 
 public class TestTemplate {
 
 	Grille g;
 	Bateau b;
-	
+	Joueur j;
 	@org.junit.Before
 	public void setup()
 	{
 		g = new Grille(5, 6);
-		b= new Bateau(1,5); //porte avion de taille 5
-		
+		b= new Bateau(1,5,); //porte avion de taille 5
+		j= new Joueur("joueur1",0,5,6);
 	}
 	
 	@org.junit.Test
@@ -24,9 +25,9 @@ public class TestTemplate {
 		
 	}
 	@org.junit.Test
-	public void test()
+	public void testCreationJoueur()
 	{
-		
+		assertNotNull(object);
 	}
 
 }
