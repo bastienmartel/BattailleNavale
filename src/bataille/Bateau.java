5f0b1c20f0f65a02b870ec2c6a3cc65d2254b14e
MARTEL BASTIEN p1404247 <p1404247@univ-lyon1.fr>
2017-03-30 11:52:45 +0200

    Ajout de tests sur bateau

--- src/bataille/Bateau.java
+++ src/bataille/Bateau.java
@@ -50,29 +50,51 @@ public class Bateau {
 	}
 
 
-	public int getX() {
-		return x;
-	}
-
-
-	public void setX(int x) {
-		this.x = x;
-	}
-
-
-	public int getY() {
-		return y;
-	}
-
-
-	public void setY(int y) {
-		this.y = y;
-	}
 
 
 	@Override
 	public String toString() {
 		return "Le " + bateau + " de taille " + taille + "entre dans le jeu"  ;
+	}
+
+
+	public int getX1() {
+		return x1;
+	}
+
+
+	public void setX1(int x1) {
+		this.x1 = x1;
+	}
+
+
+	public int getY1() {
+		return y1;
+	}
+
+
+	public void setY1(int y1) {
+		this.y1 = y1;
+	}
+
+
+	public int getX2() {
+		return x2;
+	}
+
+
+	public void setX2(int x2) {
+		this.x2 = x2;
+	}
+
+
+	public int getY2() {
+		return y2;
+	}
+
+
+	public void setY2(int y2) {
+		this.y2 = y2;
 	}
 	
 	
--- src/test/TestTemplate.java
+++ src/test/TestTemplate.java
@@ -14,16 +14,25 @@ public class TestTemplate {
 	public void setup()
 	{
 		g = new Grille(5, 6);
-		b= new Bateau(1,5,); //porte avion de taille 5
+		b= new Bateau(1,5,1,2,6,2); //porte avion de taille 5
 		j= new Joueur("joueur1",0,5,6);
 	}
 	
 	@org.junit.Test
-	public void testInsertionGrille() {
-		
-		
-		
+	public void testTailleBateauVertical() {
+		assertEquals(5,(b.getX2() - b.getX1()));
 	}
+	
+	@org.junit.Test
+	public void testTailleBateauHorizontal() {
+		assertEquals(5,(b.getY2() - b.getY1()));
+	}
+	
+	@org.junit.Test
+	public void testTypeBateau() {
+		assertEquals("Porte Avion", b.getTypeBateau());
+	}
+	
 	@org.junit.Test
 	public void testCreationJoueur()
 	{
