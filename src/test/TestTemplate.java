439e0eca422cace675bdaee0f4a451466bb8fe0e
gprog39 <pyfes@aol.com>
2017-03-31 14:29:25 +0200

    test joueurs différents implémenté

--- src/test/TestTemplate.java
+++ src/test/TestTemplate.java
@@ -10,12 +10,14 @@ public class TestTemplate {
 	Grille g;
 	Bateau b;
 	Joueur j;
+	Joueur j2;
 	@org.junit.Before
 	public void setup()
 	{
 		g = new Grille(5, 6);
 		b= new Bateau(1,5,1,2,6,2); //porte avion de taille 5
-		j= new Joueur("joueur1",0,5,6);
+		j= new Joueur("joueur1",5,6);
+		j2= new Joueur("joueur2",5,6);
 	}
 	
 	@org.junit.Test
@@ -36,7 +38,7 @@ public class TestTemplate {
 	@org.junit.Test
 	public void testCreationJoueur()
 	{
-		//assertNotNull(object);
+		
 	}
 
 }
