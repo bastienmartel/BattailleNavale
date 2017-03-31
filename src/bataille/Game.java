c24724e22d11255ebb5cf9099094c540069c77c1
gprog39 <pyfes@aol.com>
2017-03-31 14:19:57 +0200

    début main

--- src/bataille/Game.java
+++ src/bataille/Game.java
@@ -1,10 +1,21 @@
 package bataille;
 
+import java.util.Scanner;
+
 public class Game {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-			Grille plateau = new Grille(5,5);
+			
+			Scanner s;
+			s= new Scanner(System.in);
+			System.out.println("Entrez la taille horizontale de la grille : \n");
+			int width = s.nextInt();
+			System.out.println("Entrez la taille verticale de la grille : \n");
+			s.reset();
+			int height = s.nextInt();
+			Grille plateau = new Grille(width,height);
+			
+			
 			
 	}
 
